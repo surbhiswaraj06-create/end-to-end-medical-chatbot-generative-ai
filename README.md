2417fb9efeb4a892950f3c7172236a6a3940a526
surbhi swaraj <your-email@example.com>
2026-02-22 19:14:26 +0530

    folder structure added

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # end-to-end-medical-chatbot-generative-ai
+repo: https://github.com/
+conda create -n medibot python=3.10 -y
+ conda activate medibot
+pip install  -r myrequirements.txt
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup( 
+    name = 'Generative AI project',
+    version= '0.0.0',
+    author='surbhi swaraj',
+    author_email='surbhiswaraj06@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+)
